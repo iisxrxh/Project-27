4b012aa2af560185839c47459645d7881f0e4f01
iisxrxh <74046058+iisxrxh@users.noreply.github.com>
2021-02-03 20:59:37 +0530

    Update Bob.js

--- Bob.js
+++ Bob.js
@@ -1,13 +1,16 @@
 class Bob{
 
     constructor(x,y,radius){
-        var options = {isStatic: true,
-                       restitution:0.3,
+        var options = {isStatic: false,
+                       restitution:0.0,
                        friction: 0.5,
                        density : 1.2,}
-        this.body = Bodies.circle(x, y, radius, options)
+        
+        this.x = x
+        this.y = y
         this.radius = radius
-       
+        this.body = Bodies.circle(this.x, this.y, this.radius/2, options)
+        
         World.add(world,this.body)
 
     } 
@@ -19,7 +22,7 @@ class Bob{
         rotate(angle)
         ellipseMode(CENTER)
         fill("red")
-        ellipse(0,0,this.radius)
+        ellipse(0,0,this.radius, this.radius)
         pop()
 
     }
